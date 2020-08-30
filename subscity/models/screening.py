eae31caabd9d47fb9ada93966bd49347bf218340
Max Danilov <contact@maxdanilov.ru>
2020-08-30 14:26:23 +0200

    Cleanup Screening model

--- subscity/models/screening.py
+++ subscity/models/screening.py
@@ -13,7 +13,6 @@ from sqlalchemy import func
 from sqlalchemy.dialects.mysql import DATETIME
 
 from subscity.models.base import Base, DB
-from subscity.models.cinema import Cinema
 from subscity.models.movie import Movie
 from subscity.utils import get_now
 from subscity.yandex_afisha_parser import YandexAfishaParser as Yap
@@ -48,31 +47,6 @@ class Screening(Base):  # pylint: disable=no-init
         return start_day - datetime.timedelta(days=1)
 
     @staticmethod
-    def get_for_movie(movie_id: int, city: str) -> List:
-        movie_api_id = DB.session.query(Movie.api_id).filter(Movie.id == movie_id).as_scalar()
-        query = DB.session.query(Screening, Movie, Cinema)
-        query = query.filter(Screening.city == city)
-        query = query.filter(Screening.movie_api_id == movie_api_id)
-        query = query.filter(Screening.cinema_api_id == Cinema.api_id)
-        query = query.filter(Screening.movie_api_id == Movie.api_id)
-        query = query.filter(Screening.date_time > get_now(city))
-        query = query.order_by(Screening.date_time)
-        return query.all()
-
-    @staticmethod
-    def get_for_cinema(cinema_id: int, city: str) -> List:
-        cinema_api_id = DB.session.query(Cinema.api_id).filter(Cinema.id == cinema_id).as_scalar()
-        query = DB.session.query(Screening, Movie, Cinema)
-        query = query.filter(Screening.city == city)
-        query = query.filter(Screening.cinema_api_id == cinema_api_id)
-        query = query.filter(Screening.cinema_api_id == Cinema.api_id)
-        query = query.filter(Screening.movie_api_id == Movie.api_id)
-        query = query.filter(Screening.date_time > get_now(city))
-        query = query.filter(Movie.hide.is_(False))
-        query = query.order_by(Screening.date_time)
-        return query.all()
-
-    @staticmethod
     def get(cinema_api_id: str = None, movie_api_id: str = None, start_day: datetime = None,
             end_day: datetime = None, city: str = None) -> List:
         query = DB.session.query(Screening)
@@ -86,18 +60,6 @@ class Screening(Base):  # pylint: disable=no-init
             end_day = end_day.replace(hour=0, minute=0, second=0, microsecond=0)
             query = query.filter(Screening.date_time <= end_day + Yap.DAY_STARTS_AT)
         query = query.order_by(Screening.date_time)
-        return query.all()
-
-    @staticmethod
-    def get_movies_cinemas(city: str) -> List:
-        query = DB.session.query(Cinema.id.label('cinema_id'),
-                                 Movie.id.label('movie_id'))
-        query = query.filter(Screening.date_time > get_now(city))
-        query = query.filter(Screening.cinema_api_id == Cinema.api_id)
-        query = query.filter(Screening.movie_api_id == Movie.api_id)
-        query = query.filter(Movie.hide.is_(False))
-        query = query.filter(Screening.city == city)
-        query = query.distinct()
         return query.all()
 
     @staticmethod
